068dbc8f4cea1367f57da05f615876041b0d1986
Peijun Zhu <zpj.ustc@gmail.com>
2017-11-04 22:31:59 -0400

    comment server

--- server/handle_with_curl.c
+++ server/handle_with_curl.c
@@ -16,17 +16,32 @@
 
 #define BUFSIZE (8803)
 
-size_t send_chunk(void *ptr, size_t size, size_t nmemb, void *data){
-    size_t realsize = size*nmemb;
-    gfcontext_t *ctx =(gfcontext_t *)data;
+/**
+ * @brief send_chunk Callback function for CURLOPT_WRITEFUNCTION to send a chunk
+ * @param ptr   pointer to data
+ * @param size  size of each element
+ * @param nmemb number of elements
+ * @param data  gfcontext_t pointer
+ * @return
+ */
+size_t send_chunk(void *ptr, size_t size, size_t nmemb, void *data) {
+    size_t realsize = size * nmemb;
+    gfcontext_t *ctx = (gfcontext_t *)data;
     ssize_t write_len = gfs_send(ctx, (char*)ptr, realsize);
-    if(write_len!=(ssize_t)realsize){
+    if(write_len != (ssize_t)realsize) {
         fprintf(stderr, "Send chunk error\n");
         return 0;
     }
     return realsize;
 }
-/*
+
+/**
+ * @brief handle_with_curl Use curl to handle file request
+ * @param ctx
+ * @param path Path of requested file
+ * @param arg Data directory
+ * @return Transfered bytes
+ *
  * Use libcurl to intercept received data of libcurl and send it to server
  */
 ssize_t handle_with_curl(gfcontext_t *ctx, char *path, void* arg) {
@@ -42,26 +57,24 @@ ssize_t handle_with_curl(gfcontext_t *ctx, char *path, void* arg) {
     curl_easy_setopt(myHandle, CURLOPT_NOBODY, 1);
     curl_easy_setopt(myHandle, CURLOPT_URL, buffer);
     result = curl_easy_perform(myHandle);
-    if(result==CURLE_OK){
+    if(result == CURLE_OK) {
         curl_easy_getinfo(myHandle, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &length);
         curl_easy_getinfo(myHandle, CURLINFO_RESPONSE_CODE, &status);
-    }
-    else{
+    } else {
         fprintf(stderr, "Error happened during curl_easy_perform of header\n");
         return -1;
     }
 
-    if(status>=400){
+    if(status >= 400) {
         gfs_sendheader(ctx, GF_FILE_NOT_FOUND, 0);
-    }
-    else{
+    } else {
         gfs_sendheader(ctx, GF_OK, (size_t)length);
     }
     curl_easy_setopt(myHandle, CURLOPT_NOBODY, 0);
     curl_easy_setopt(myHandle, CURLOPT_WRITEFUNCTION, send_chunk);
     curl_easy_setopt(myHandle, CURLOPT_WRITEDATA, (void *)ctx);
     result = curl_easy_perform(myHandle);
-    if(result!=CURLE_OK){
+    if(result != CURLE_OK) {
         fprintf(stderr, "Error happened during curl_easy_perform\n");
         return -1;
     }
